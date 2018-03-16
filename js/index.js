3b1411fb6e7f595e7e708e8840f2e404bf8e38f2
t3liph0n3 <shane@tokoro.localdomain>
2018-03-16 22:15:04 +0800

    bare bones game - with buy, sell, move function

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 var nowPort = 0;
 // ammount of cash held
 var cash = 100;
+//array for cargo of ship
+var cargo = [];
 
 function moveShip(){
   // Move the ship to the next port and update the port information in the table
@@ -10,35 +12,73 @@ function moveShip(){
  }else{
    nowPort = 0;
  }
-  document.getElementById("port").innerHTML = "now in " + portName[nowPort];
+  document.getElementById("port").innerHTML = "Now in " + portName[nowPort] + " port.";
   
-  document.getElementById("portTax").innerHTML = portInfo[nowPort][0]*100 + "% port landing tax.";
+  document.getElementById("portTax").innerHTML = "Where import tarrifs are: "+ portInfo[nowPort][0]*100 + "%.";
     document.getElementById("Fert").innerHTML = portInfo[nowPort][1];
     document.getElementById("Cere").innerHTML = portInfo[nowPort][2];
     document.getElementById("Tool").innerHTML = portInfo[nowPort][3];
     document.getElementById("Booz").innerHTML = portInfo[nowPort][4];
 }
 
-function Buy(stock) {
+function buyCargo(stock) {
   //buy produce from the current port, then update cash and hold info if good sale
-  if (cargo.length < 4){
+  if (cargo.length < 4 && cash > portInfo[nowPort][stock]){
     cash -= portInfo[nowPort][stock];
     document.getElementById("cash").innerHTML = "$" + cash;
     cargo[cargo.length] = stock;
-    document.getElementById("hold").innerHTML = "<ul><li>"+ cargo[0] + "</li><li>" + cargo[1] + "</li><li>" + cargo[2] + "</li><li>" + cargo[3] +"</li></ul>";
+    listCargo();
   } else {
     //show error can not buy
-    alert("You don't have any empty hold");
+    if (cargo.length >= 4){
+      alert("You don't have enough hold for more cargo. \n Sell some cargo to make room.");
+    } else {
+      alert("You don't have enough cash for " + cargoList[stock] + " here.");
+    }
   }
 }
 
-//array for cargo of ship
-var cargo = []
+
+function sellCargo(hld) {
+  //sell produce in the selected hold, add to cash price - landing tax
+  //set hold to undefined (to be filled on buy)
+  switch (cargo[hld]) {
+    case 0:
+    case undefined:
+      alert("That hold is empty.");
+      break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      var sellPrice = portInfo[nowPort][cargo[hld]] * (1-portInfo[nowPort][0]);
+      cash += sellPrice;
+      cash -= cash%1; //this is done to round the ammount down to whole $
+      document.getElementById("cash").innerHTML = "$" + cash;
+      cargo.pop(cargo[hld]);
+      break;
+  }
+  listCargo();
+}
+
+function listCargo() {
+  //List the cargo in the hold
+  if (cargo.length < 1){
+  document.getElementById("hold").innerHTML = "empty";
+  } else {
+    document.getElementById("hold").innerHTML = "<ul><li>"+ cargo.join("</li><li>") + "</li></ul>";
+  }
+}
 
       
 //The list of 4 port names
 var portName = [
   "Alice", "Buck", "Cloud", "Domino"
+]
+
+//The list of the items (notused,ItemA, ItemB, ItemC, ItemD)
+var cargoList = [
+  "empty","Fertiliser","Cereal","Tools","Booze"
 ]
 
 //A nested table of Port information
