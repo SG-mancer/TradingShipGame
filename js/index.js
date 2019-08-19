a79a22259f718638f08dc8f59f8ee82a16fb3d46
t3liph0n3 <hyperstn@outlook.jp>
2019-08-19 11:50:35 +0800

    recursions

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 // ammount of cash held
-var cash = 100;
+var cash = 10000000;
 // number of turns - used in reporting game over
 var turns = 0;
+var maxCom = 10; //from html - how many ComL# and Comd#... max length of Cargo List
 //array for cargo of ship
 var cargo = [];
-var maxCargo = 6; //The maximum cargo that can be held
-//The list of 7 port names (country fort, windward island, wicked hill, reef, hollow island, rabbit town , cliff village)
-var portName = [
+var maxCargo = 5; //The maximum cargo that can  be held
+
+var portName = [ //Ports for the game (check PortInfo has same number)
   "Peyi-fo", "Lildivan", "Modi Mon", "Kay", "Kwez Lilet", "Lapen-vil", "Falez-vilaj"
 ]
 //The list of the items
-//  note the length of this list is used for printing prices,
-//  and needs to be reflected in the index.html ComL# and Comd# ids.
+// max length is 10 (from html doc)
 var cargoList = [
   "empty","Fish","Coffee","Sugar","Coconuts","Iron","Coco"
 ]
 //A nested table of Port information
-// [ Tax, Item A, Item B, Item C, Item D ]
+// [ Tax, Item A, Item B, Item C, Item D (in cargo list) ]
 var portInfo = [
   [0.40,5,51,22,12,135,610], //Payi-fo cheap coco
   [0.20,2,60,24,10,140,2070], //Lildivan cheap coconuts
@@ -33,6 +33,7 @@ var nowPort = Math.floor(Math.random() * portInfo.length);
 // to start the game, print lables then move the ship to start
 printlables();
 moveShip();
+
 
 //move Ship is each turn...
 function moveShip(){
@@ -52,33 +53,49 @@ function moveShip(){
   turns += 1;
 }
 
+
 // show the commodity names (called at start of game)
 function printlables() {
-  for (var i = 1; i < cargoList.length; i++){
-    document.getElementById("ComL"+i).innerHTML = cargoList[i];
+  for (var i = 1; i <= maxCom; i++){
+    if (i < cargoList.length) {
+      document.getElementById("ComL"+i).innerHTML = cargoList[i];
+    } else {
+      document.getElementById("ComL"+i).innerHTML = "";
+    }
   }
 }
 
+
+//update the information on the table for the port
 function listPort() {
- //update the information on the table for the port
   document.getElementById("port").innerHTML = "Now in " + portName[nowPort] + ".";
 
   document.getElementById("portTax").innerHTML = "Where import tarrifs are: "+ Math.round(portInfo[nowPort][0]*100) + "%.";
   //update table with prices
-  for (var i = 1; i < cargoList.length; i++){
-    document.getElementById("Comd"+i).innerHTML = portInfo[nowPort][i];
+  for (var i = 1; i <= maxCom; i++){
+    if (i < cargoList.length) {
+      document.getElementById("Comd"+i).innerHTML = portInfo[nowPort][i]+"<br//><button type=\"button\" onclick=\"buyCargo("+i+")\">Buy</button>";
+    } else {
+      document.getElementById("Comd"+i).innerHTML = "";
+    }
   }
 }
 
+
+//buy produce, update cash, cost and hold info if good sale
 function buyCargo(stock) {
-  //buy produce from the current port, then update cash and hold info if good sale
   if (cargo.length < maxCargo && cash >= portInfo[nowPort][stock]){
-    cash -= portInfo[nowPort][stock];
-    document.getElementById("cash").innerHTML = "$" + cash;
-    cargo[cargo.length] = stock;
-    portInfo[nowPort][stock] += Math.round(portInfo[nowPort][stock]*(Math.floor(Math.random() * 25))/100);
-    listCargo();
-    listPort();
+    if (portInfo[nowPort][stock] > 0){
+      cash -= portInfo[nowPort][stock];
+      document.getElementById("cash").innerHTML = "$" + cash;
+      cargo[cargo.length] = stock;
+      //price will rise 1-14%
+      portInfo[nowPort][stock] = Math.round(portInfo[nowPort][stock]*(Math.random()*15+100)/100);
+      listCargo();
+      listPort();
+    } else {
+      alert("sorry out of stock");
+    }
   } else {
     //show error can not buy
     if (cargo.length >= maxCargo){
@@ -90,6 +107,7 @@ function buyCargo(stock) {
 }
 
 
+//Sell cargo, check if legal, update cash and cost
 function sellCargo(hld) {
   //sell produce in the selected hold, add to cash price - landing tax
   //set hold to undefined (to be filled on buy)
@@ -98,16 +116,12 @@ function sellCargo(hld) {
     case undefined:
       alert("That hold is empty.");
       break;
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
+    default:
       var sellPrice = portInfo[nowPort][cargo[hld]] * (1-portInfo[nowPort][0]);
       cash += Math.round(sellPrice);
       document.getElementById("cash").innerHTML = "$" + cash;
-      portInfo[nowPort][cargo[hld]] -= Math.round(portInfo[nowPort][cargo[hld]]*(Math.floor(Math.random() * 20))/100);
+      //price will drop 0-5%
+      portInfo[nowPort][cargo[hld]] = Math.round(portInfo[nowPort][cargo[hld]]*(Math.random()*5+95)/100);
       cargo.pop(cargo[hld]);
       listPort();
       break;
@@ -117,33 +131,40 @@ function sellCargo(hld) {
 
 function listCargo() {
   //List the cargo in the hold
+  var cHold = 0; //used for adding the Sell button
   if (cargo.length < 1){
   document.getElementById("hold").innerHTML = "empty";
   } else {
-
     document.getElementById("hold").innerHTML = "<ul><li>"+ cargo.map(element => {
-  return cargoList[element];
+  return cargoList[element]+" <button type\"button\" onclick=\"sellCargo("+(cHold++)+")\"> Sell </button>";
 }).join("</li><li>") + "</li></ul>";
+  }
+
+  for (var i=0; i <=portInfo.length;i++){
+    alert(portInfo[i][0]);
   }
 }
 
 function priceChange () {
   // Change the tax or price of goods in a random port and report it as news.
-  var effectPort = Math.floor(Math.random() * portInfo.length);
-  var effectCargo = Math.floor(Math.random() * 4)+1;
+  var effectPort = Math.floor(Math.random()*portInfo.length);
+  var effectCargo = Math.floor(Math.random()*cargoList.length);
+  if (effectCargo == 0){ //check not effecting tax
+     effectCargo = 1;
+  }
   switch(Math.floor(Math.random() * 4)){
-    case 0:   //change tax
-      portInfo[effectPort][0] += 0.01;
-      portInfo[effectPort][0] -= portInfo[effectPort][0]%0.01; //to round?
-      document.getElementById("gamenews").innerHTML = "tax increase in "+portName[effectPort]+"!";
+    case 0:   //change tax 1-6%
+      portInfo[effectPort][0] = Math.floor(portInfo[effectPort][0]*100 + 1 + Math.floor(Math.random()*5))/100;
+      portInfo[effectPort][0] -= portInfo[effectPort][0]%0.01; //to round
+      document.getElementById("gamenews").innerHTML = "Tarrif increase in "+portName[effectPort]+"! Now: "+portInfo[effectPort][0]*100+"%";
       break;
     case 1:  //price decrease
-      portInfo[effectPort][effectCargo] -= Math.round(portInfo[effectPort][effectCargo]*(Math.floor(Math.random() * 10))/100);
-      document.getElementById("gamenews").innerHTML = "Surplus "+cargoList[effectCargo]+" at " +portName[effectPort]+ ".";
+      portInfo[effectPort][effectCargo] -= 1+Math.round(portInfo[effectPort][effectCargo]*(Math.random()*15+20)/100);
+      document.getElementById("gamenews").innerHTML = "Price collapse of "+cargoList[effectCargo]+" at " +portName[effectPort]+ ". Now: $"+portInfo[effectPort][effectCargo]+" a unit.";
       break;
     case 2: //price increase
-      portInfo[effectPort][effectCargo] += Math.round(portInfo[effectPort][effectCargo]*(Math.floor(Math.random() * 10))/100);
-      document.getElementById("gamenews").innerHTML = "Shortage of "+cargoList[effectCargo]+" at " +portName[effectPort]+ ".";
+      portInfo[effectPort][effectCargo] += 2+Math.round(portInfo[effectPort][effectCargo]*(Math.random()*10)/100);
+      document.getElementById("gamenews").innerHTML = "Shortage of "+cargoList[effectCargo]+" at " +portName[effectPort]+ ".Now: $"+portInfo[effectPort][effectCargo]+" a unit.";
       break;
     case 3: //pirates steal money if in same location
       if (effectPort == nowPort) {
